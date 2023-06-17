bc3e7f3c468ea5efc8e0459a246e6b8feb211fd9
JaiderMartinez <smithsaemirto45@gmail.com>
2023-06-16 23:01:55 -0500

    estructura

---
+++ src/main/java/com/reto/twilio/MensajeriaApplication.java
@@ -0,0 +1,13 @@
+package com.reto.twilio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MensajeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MensajeriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reto/twilio/MensajeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reto.twilio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MensajeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
